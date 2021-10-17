88a9bae4ccef8defb71a492aaa4d80a163dcb8bc
GTsung <xc_goo@163.com>
2021-10-17 12:35:27 +0800

    scan

--- small-step-13/src/main/java/com/context/annotation/ClassPathScanningCandidateComponentProvider.java
+++ small-step-13/src/main/java/com/context/annotation/ClassPathScanningCandidateComponentProvider.java
@@ -1,8 +1,24 @@
 package com.context.annotation;
 
+import cn.hutool.core.util.ClassUtil;
+import com.home.beans.factory.factory.BeanDefinition;
+import com.stereotype.Component;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  * @author GTsung
  * @date 2021/10/17
  */
 public class ClassPathScanningCandidateComponentProvider {
+
+    public Set<BeanDefinition> findCandidateComponents(String basePackage) {
+        Set<BeanDefinition> candidates = new LinkedHashSet<>();
+        Set<Class<?>> classes = ClassUtil.scanPackageByAnnotation(basePackage, Component.class);
+        for (Class clazz :classes) {
+            candidates.add(new BeanDefinition(clazz));
+        }
+        return candidates;
+    }
 }
