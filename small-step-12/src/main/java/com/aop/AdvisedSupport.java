48c2369e5f2bbfc3044ed60364f32c53dcb054ec
GTsung <xc_goo@163.com>
2021-10-17 11:51:18 +0800

    aop

--- small-step-12/src/main/java/com/aop/AdvisedSupport.java
+++ small-step-12/src/main/java/com/aop/AdvisedSupport.java
@@ -7,12 +7,24 @@ import org.aopalliance.intercept.MethodInterceptor;
  * @date 2021/10/16
  */
 public class AdvisedSupport {
+
+    // ProxyConfig
+    private boolean proxyTargetClass = false;
+
     // 被代理的目标对象
     private TargetSource targetSource;
     // 方法拦截器
     private MethodInterceptor methodInterceptor;
     // 方法匹配器(检查目标方法是否符合通知条件)
     private MethodMatcher methodMatcher;
+
+    public boolean isProxyTargetClass() {
+        return proxyTargetClass;
+    }
+
+    public void setProxyTargetClass(boolean proxyTargetClass) {
+        this.proxyTargetClass = proxyTargetClass;
+    }
 
     public TargetSource getTargetSource() {
         return targetSource;
---
+++ small-step-12/src/main/java/com/home/beans/factory/factory/InstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package com.home.beans.factory.factory;
+
+import com.home.beans.BeansException;
+
+/**
+ * @author GTsung
+ * @date 2021/10/17
+ */
+public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
+
+    /**
+     * Apply this BeanPostProcessor <i>before the target bean gets instantiated</i>.
+     * The returned bean object may be a proxy to use instead of the target bean,
+     * effectively suppressing default instantiation of the target bean.
+     *
+     * 在 Bean 对象执行初始化方法之前，执行此方法
+     *
+     * @param beanClass
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException;
+}
